34a75f58849b96c98e371ebb672d21e90b4c5013
Dhanesh Mane <dmane@mediamath.com>
2023-07-15 20:34:23 +0530

    ui improvements

--- app/src/list/List.js
+++ app/src/list/List.js
@@ -1,7 +1,13 @@
 import "./list.css";
 import { Accordion } from "react-bootstrap";
+import { age, getMyAvatar, getMyName } from "../util/util.js";
+import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
 
 const widthOfAccordion = "50%";
+
+const marginRight20 = {
+  marginTop: "20px",
+};
 
 const List = (props) => {
   const { celebrityList } = props;
@@ -9,17 +15,43 @@ const List = (props) => {
   const renderList = () => {
     const createItem = (item, index) => {
       console.log(item);
+      const dob = item?.dob?.split("-");
+      const birthdate = new Date(...dob);
+      let myAge = age(birthdate);
+
       return (
         <Accordion.Item eventKey={index} className="item">
-          <Accordion.Header>{`${item.first} ${item.last}`}</Accordion.Header>
+          <Accordion.Header>
+            {getMyAvatar(item.picture)}
+            {getMyName(item.first, item.last)}
+          </Accordion.Header>
           <Accordion.Body>
-            Age: {item.age}
-            Gender: {item.gender}
-            Country: {item.country}
-            <hr />
-            Description:
-            <br />
-            {item.description}
+            <div class="container">
+              <div class="row">
+                <div class="col text-left text-muted">Age</div>
+                <div class="col text-left text-muted">Gender</div>
+                <div class="col text-left text-muted">Country</div>
+              </div>
+              <div class="row">
+                <div class="col text-left">{myAge}</div>
+                <div class="col text-left">{item.gender}</div>
+                <div class="col text-left">{item.country}</div>
+              </div>
+              <div class="row" style={marginRight20}>
+                <div class="col text-left text-muted">Description</div>
+              </div>
+              <div class="row">
+                <div class="col text-left">{item.description}</div>
+              </div>
+              <div class="row" style={marginRight20}>
+                <div class="col text-right">
+                  <BsFillTrashFill
+                    style={{ marginRight: "10px", color: "red" }}
+                  />
+                  <BsFillPencilFill style={{ color: "blue" }} />
+                </div>
+              </div>
+            </div>
           </Accordion.Body>
         </Accordion.Item>
       );
